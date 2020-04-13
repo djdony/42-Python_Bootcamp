4ddc1aa9a5045919f8d774bcfb266ae6c2936e38
dony <djdony@mail.ru>
2020-04-13 17:27:20 +0300

    fix_ex01

--- d00/ex01/exec.py
+++ d00/ex01/exec.py
@@ -4,7 +4,14 @@ args = sys.argv
 
 
 def reverse(s):
-    return s[::-1]
+    str_reverse = s[::-1]
+    str1 = ''
+    for i in str_reverse:
+        if i.isupper():
+            str1 += i.lower()
+        else:
+            str1 += i.upper()
+    return str1
 
 
 for arg in reversed(args[1:]):
